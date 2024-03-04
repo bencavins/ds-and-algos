4f3a7ef640cebfd9545df9bd57f01afeb191d9c6
Ben Cavins <ben.cavins@flatironschool.com>
2024-03-04 11:50:31 -0500

    m

--- data_structures/queue.py
+++ data_structures/queue.py
@@ -1,2 +1,13 @@
+from linked_list import LinkedList
+
 class Queue:
-    pass
+    def __init__(self):
+        self.data = LinkedList()
+
+    def enqueue(self, value):
+        """Add value to the back of the line"""
+        self.data.add_back(value)  # O(1)
+
+    def dequeue(self):
+        """Remove from the front of the line"""
+        return self.data.delete_front()  # O(1)
--- data_structures/stack.py
+++ data_structures/stack.py
@@ -1,2 +1,13 @@
+# LIFO (Last in, First Out)
+
 class Stack:
-    pass
+    def __init__(self):
+        self.data = []
+
+    def push(self, value):
+        """Add value to the top of the stack"""
+        self.data.append(value)  # O(1)
+
+    def pop(self):
+        """Remove value from the top of the stack"""
+        return self.data.pop()  # O(1)
