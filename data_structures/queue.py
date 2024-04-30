6d9ff4170b08a06571976ea9c01185260f56e511
Ben Cavins <ben.cavins@flatironschool.com>
2024-04-30 12:08:18 -0400

    add stacks/queues

--- data_structures/queue.py
+++ data_structures/queue.py
@@ -1,2 +1,14 @@
+from linked_list import LinkedList
+# first in, first out (FIFO)
+
 class Queue:
-    pass
+    def __init__(self) -> None:
+        self.data = LinkedList()
+
+    def enqueue(self, value):  # O(1)
+        """Add value to back of queue"""
+        self.data.add_back(value)
+
+    def dequeue(self):  # O(1)
+        """Remove from front of queue"""
+        return self.data.delete_front()
--- data_structures/stack.py
+++ data_structures/stack.py
@@ -1,2 +1,18 @@
+# Last in, First out (LIFO)
+
 class Stack:
-    pass
+    def __init__(self) -> None:
+        self.data = []
+
+    def push(self, value): # O(1)
+        """Adds a new value to the top of the stack"""
+        self.data.append(value)
+
+    def pop(self): # O(1)
+        """Removes the value from the top of the stack"""
+        return self.data.pop()
+    
+    def peek(self):
+        return self.data[-1]
+
+s = Stack()
